22598803309996cdbc84afec4e2f2192b7acb9d0
via-shcherba <via.shcherba@gmail.com>
2019-03-12 10:59:08 +0300

    feat: add implementation for func warmup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function warmup(temperature) {
-  // your implementation here
+  if(typeof(temperature) == "number"){	
+		return 1.8 * temperature + 32;
+	}else{
+		return null;
+	}
 };
